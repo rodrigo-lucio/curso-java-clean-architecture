a3dd641968ca1f5e879cd37718a13884eb017b1c
Rodrigo Lúcio <luciodigo@gmail.com>
2022-07-14 23:08:32 -0300

    Criação dos testes unitários

---
+++ src/test/java/br/com/lucio/EmailTest.java
@@ -0,0 +1,27 @@
+package br.com.lucio;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class EmailTest {
+
+	@Test
+	void naoDeveCriarEmailComEnderecosInvalidos() {
+		assertThrows(IllegalArgumentException.class, () -> new Email(null));
+		assertThrows(IllegalArgumentException.class, () -> new Email("blabla"));
+		assertThrows(IllegalArgumentException.class, () -> new Email(""));
+	}
+
+	@Test
+	void deveCriarEmailComEnderecosValidos() {
+		assertDoesNotThrow(() -> new Email("luciodigo@gmail.com"));
+		assertDoesNotThrow(() -> new Email("luciodigo@gmail.com.br"));
+		assertDoesNotThrow(() -> new Email("luciodigo@gmail.br"));
+		assertDoesNotThrow(() -> new Email("luciodigo@gmail.gov.br"));
+		assertDoesNotThrow(() -> new Email("luciodigo@lucio.com.br"));
+		assertDoesNotThrow(() -> new Email("luciodigo@lucio.net.br"));
+	}
+
+}
