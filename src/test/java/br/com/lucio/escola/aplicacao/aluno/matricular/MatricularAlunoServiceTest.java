05fbeb5789c2d618ed23b679c07c6159cae45ffe
Rodrigo Lúcio <luciodigo@gmail.com>
2022-07-26 21:58:22 -0300

    Ajustado teste unitário para verificar se o publicador emitiu os eventos

--- src/test/java/br/com/lucio/escola/aplicacao/aluno/matricular/MatricularAlunoServiceTest.java
+++ src/test/java/br/com/lucio/escola/aplicacao/aluno/matricular/MatricularAlunoServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.lucio.escola.aplicacao.aluno.matricular;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,34 +11,40 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.lucio.escola.dominio.Evento;
+import br.com.lucio.escola.dominio.PublicadorEventos;
 import br.com.lucio.escola.dominio.aluno.Aluno;
 import br.com.lucio.escola.dominio.aluno.AlunoRepository;
 
 @ExtendWith(MockitoExtension.class)
 class MatricularAlunoServiceTest {
 
-    @InjectMocks
-    private MatricularAlunoService service;
+	@InjectMocks
+	private MatricularAlunoService service;
 
-    @Mock
-    private AlunoRepository alunoRepository;
+	@Mock
+	private AlunoRepository alunoRepository;
 
-    private MatricularAlunoDTO dto;
+	@Mock
+	private PublicadorEventos publicadorEventos;
 
-    @BeforeEach
-    void setUp() {
-        dto = MatricularAlunoDTO.builder() //
-                .nome("Rodrigo Lúcio") //
-                .cpf("31157399070") //
-                .email("luciodigo@gmail.com") //
-                .build(); //
-    }
+	private MatricularAlunoDTO dto;
 
-    @Test
-    void alunoDeveSerPersistido() {
-        Aluno aluno = dto.toAluno();
-        assertDoesNotThrow(() -> service.executa(aluno));
-        verify(alunoRepository).matricular(aluno);
-    }
+	@BeforeEach
+	void setUp() {
+		dto = MatricularAlunoDTO.builder() //
+				.nome("Rodrigo Lúcio") //
+				.cpf("31157399070") //
+				.email("luciodigo@gmail.com") //
+				.build(); //
+	}
+
+	@Test
+	void alunoDeveSerPersistido() {
+		Aluno aluno = dto.toAluno();
+		assertDoesNotThrow(() -> service.executa(aluno));
+		verify(alunoRepository).matricular(aluno);
+		verify(publicadorEventos).publicar(any(Evento.class));
+	}
 
 }
